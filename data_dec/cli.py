b0220e0437eb600eeb8713ec3a7ecb05a758ce12
kentkr <kyle.kent321@gmail.com>
2024-08-13 18:06:43 -0500

    tidying up: delete db.py, add type hints

--- data_dec/cli.py
+++ data_dec/cli.py
@@ -3,9 +3,9 @@ import os
 import re
 import argparse
 from databricks.connect import DatabricksSession
-from data_dec.entity import Entity, Model
-from pyspark.sql import Row, DataFrame
+from data_dec.entity import Entity 
 
+# parse arguments
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest='command', required=True)
 build_parser = subparsers.add_parser('build', help='Run and test models')
@@ -17,11 +17,12 @@ args = parser.parse_args()
 # global spark session - still useful to call in each model script
 spark = DatabricksSession.builder.profile("dev").getOrCreate()
 
-# import entities
+# base entity import
 entity = Entity()
 
+# Loop through each model file and execute - this loads the entity class
 def load_models() -> None:
-    models_dir = os.path.abspath(os.path.join(__file__, '../../tmp_project/models/'))
+    models_dir = os.path.abspath(os.path.join(__file__, '../../project/models/'))
     is_py_file = re.compile(r'\.py$')
     files = os.listdir(models_dir)
     for file in files:
@@ -29,19 +30,23 @@ def load_models() -> None:
             with open(os.path.join(models_dir, file), 'r') as file:
                 exec(file.read())
 
+# loop through each model, write it to db
 def run() -> None:
     for model in entity.models.values():
         model.write()
 
+# loop through each model, test it's output
 def test() -> None:
     for model in entity.models.values():
         model.test()
 
+# loop through each model, write then test it
 def build() -> None:
     for model in entity.models.values():
         model.write()
         model.test()
 
+# cli entry point
 def main():
     load_models()
     if args.command == 'build':
--- data_dec/entity.py
+++ data_dec/entity.py
@@ -1,40 +1,43 @@
 
 from collections import defaultdict
 import functools
-from typing import Callable
-from dataclasses import dataclass
+from typing import Callable, List
 from pyspark.sql import DataFrame
 
 class Model:
+    """Model class. Stores model metadata and can write/test a model"""
     def __init__(self, path: str, fn: Callable[[], DataFrame]) -> None:
         self.path = path
         self.fn = fn
         self.name = fn.__name__
 
     def write(self) -> None:
+        """Save model as spark table"""
         print(f'Writing model {self.name!r} to table {self.path!r}')
         self.fn().write.mode('overwrite').saveAsTable(self.path)
 
     def test(self) -> None:
+        """Loop through tests for this function and test"""
         entities = Entity
         print(f'Testing model {self.name}')
         for test in entities.tests[self.name]:
             print(f'Testing: {test.__name__!r}')
-            print(test(self.fn))
+            print(test(self))
 
 
 class Test:
+    """Static functions that take a model and test it"""
     @staticmethod
-    def not_empty(fn: Callable[[], DataFrame]):
-        df = fn()
+    def not_empty(model: Model):
+        df = model.fn() # this calls the original function of the model
         if len(df.collect()) > 0:
             return 'Test passes'
         else:
             return 'Test fails'
 
     @staticmethod
-    def not_null(fn: Callable[[], DataFrame]):
-        df = fn()
+    def not_null(model: Model):
+        df = model.fn()
         rows = df.collect()
         # this is an example
         first_col = rows[0].__fields__[0]
@@ -45,17 +48,28 @@ class Test:
 
 
 class Entity:
-    models = {}
-    tests = defaultdict(list)
-    references = defaultdict(list)
-    # no argument needed or possible
+    """
+    Entities class to keeps track of all models, tests, and references.
+
+    The methods are used as decorators.
+    """
+    models: dict[str, Model] = {}
+    tests: dict[str, List[Callable]] = defaultdict(list)
+    references: dict[str, List[str]] = defaultdict(list)
+
     @classmethod
-    def register_model(cls, path: str):
-        def decorator(fn):
+    def register_model(cls, path: str) -> Callable:
+        """
+        Register a model. Model names are the name of the function. Path is where it will get
+        written to
+        """
+        def decorator(fn: Callable[[], DataFrame]) -> Callable:
+            # this keeps __name__ and __doc__ related to the wrapped function
             @functools.wraps(fn)
-            def wrapper(*args, **kwargs):
+            # this always returns the decorated function, no modifications
+            def wrapper(*args, **kwargs) -> DataFrame:
                 return fn(*args, **kwargs)
-            # Assign name if not provided
+            # Create a Model and assign it to models
             model = Model(path=path, fn=fn)
             cls.models[fn.__name__] = model
             return wrapper
@@ -63,10 +77,13 @@ class Entity:
 
     # will only work with register_model
     @classmethod
-    def register_test(cls, test_name):
-        def decorator(fn):
+    def register_test(cls, test_name: str) -> Callable:
+        """
+        Register a test. Models use tests later when testing.
+        """
+        def decorator(fn: Callable) -> Callable:
             @functools.wraps(fn)
-            def wrapper(*args, **kwargs):
+            def wrapper(*args, **kwargs) -> DataFrame:
                 return fn(*args, **kwargs)
             test_function = Test.__dict__[test_name]
             cls.tests[fn.__name__].append(test_function)
@@ -74,14 +91,16 @@ class Entity:
         return decorator
 
     @classmethod
-    def register_reference(cls, reference: str):
-        def decorator(fn):
+    def register_reference(cls, reference: str) -> Callable:
+        """
+        Register a reference. This can be another model or source, but is not being used yet.
+        """
+        def decorator(fn: Callable) -> Callable:
             @functools.wraps(fn)
-            def wrapper(*args, **kwargs):
+            def wrapper(*args, **kwargs) -> DataFrame:
                 return fn(*args, **kwargs)
             cls.references[reference].append(fn.__name__)
             return wrapper
         return decorator
-
 
 

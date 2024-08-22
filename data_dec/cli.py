b354bcd4fc6aae376b42809b03f21ca79059e76c
kentkr <kyle.kent321@gmail.com>
2024-08-22 10:28:05 -0500

    --select flag

--- data_dec/cli.py
+++ data_dec/cli.py
@@ -6,18 +6,22 @@ from data_dec.runner import ProjectRunner
 from data_dec.configuration import Project
 
 # parse arguments
-def parse_args() -> argparse.Namespace:
+def parse_args():
     parser = argparse.ArgumentParser()
-    # group together basic commands like build or test
-    subparsers = parser.add_subparsers(dest='command', required=True)
-    build_parser = subparsers.add_parser('build', help='Run and test models')
-    run_parser = subparsers.add_parser('run', help='Run models')
-    test_parser = subparsers.add_parser('test', help='Test models')
-    draw_parser = subparsers.add_parser('draw', help='Draw the DAG')
-    parser.add_argument('--profiles-dir', help='Directory with your profiles.yml')
-    parser.add_argument('--project-dir', help='Directory of your data-dec project')
-    args = parser.parse_args()
-    return args
+    # args applied to whole script
+    parser.add_argument('--profiles-dir', help='Directory with your profiles.yml', required=False)
+    parser.add_argument('--project-dir', help='Directory of your data-dec project', required=False)
+    # subparser for commands
+    subparsers = parser.add_subparsers(dest='commands', required=True)
+    # command plags
+    command_flags = argparse.ArgumentParser(add_help=False)
+    command_flags.add_argument('-s', '--select', help='Select nodes to act on')
+    # subparser with command flags
+    build_parser = subparsers.add_parser('build', help='Run and test models', parents=[command_flags])
+    run_parser = subparsers.add_parser('run', help='Run models', parents=[command_flags])
+    test_parser = subparsers.add_parser('test', help='Test models', parents=[command_flags])
+    draw_parser = subparsers.add_parser('draw', help='Draw the DAG', parents=[command_flags])
+    return parser.parse_args()
 
 # cli entry point
 def main() -> None:
